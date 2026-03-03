765cf2a535f560d966f85e8293f1edd4e3061e91
JLSD3v <jl.s.d3v@gmail.com>
2026-03-03 19:24:12 +0000

    Nivel Aventureiro 2 Concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@ float Area, PIB, Area2, PIB2, densidade, densidade2, capita, capita2;
 char Cidade[30], Codigo[4], Cidade2[30], Codigo2[4];
 char Estado, Estado2;
 float superpoder1, superpoder2;
+int opcoes;
 
 
 //Primeira Carta
@@ -67,7 +68,6 @@ capita2 = (float)PIB2 / Populacao2;
 // Cálculo do Super Poder
 superpoder1 = (float)Populacao + Area + PIB + (float)Pontos_Turisticos + capita + (Area / (float)Populacao);
 superpoder2 = (float)Populacao2 + Area2 + PIB2 + (float)Pontos_Turisticos2 + capita2 + (Area2 / (float)Populacao2);
-
 //Separador para melhor visualização
 printf("----------------------------------------------------------\n");
 
@@ -78,7 +78,7 @@ printf("Código:%s\nNome da Cidade: %s\n", Codigo, Cidade);
 printf("População:%lu\n", Populacao);  
 printf("Área: %.2f km²\nPIB:R$%.2f reais\n", Area, PIB);
 printf("Número de Pontos Turísticos:%d\n", Pontos_Turisticos);
-printf("Densidade Populacional:%.2f hab/km2\n", densidade);
+printf("Densidade Demográfica:%.2f hab/km2\n", densidade);
 printf("PIB per Capita:R$%.2f reais\n", capita);
 printf("Super Poder: %.2f\n", superpoder1);
 
@@ -92,50 +92,65 @@ printf("Código:%s\nNome da Cidade:%s\n", Codigo2, Cidade2);
 printf("População:%lu\n", Populacao2);  
 printf("Área:%.2f km²\nPIB:R$%.2f reais\n", Area2, PIB2);
 printf("Número de Pontos Turísticos:%d\n", Pontos_Turisticos2);
-printf("Densidade Populacional:%.2f hab/km2\n", densidade2);
+printf("Densidade Demográfica:%.2f hab/km2\n", densidade2);
 printf("PIB per Capita:R$%.2f reais\n", capita2);
 printf("Super Poder: %.2f\n", superpoder2);
 
 //Separador para melhor visualização
 printf("----------------------------------------------------------\n");
 
-//Comparação de Cartas
-printf("Comparação de Cartas:\n");
+
+
+//Menu interativo
+printf("1-População\n");
+printf("2-Área\n");
+printf("3-PIB\n");
+printf("4-Pontos turísticos\n");
+printf("5-Desnsidade demográfica\n");
+printf("Escolha qual atributo você deseja comparar:");
+scanf("%d", &opcoes);
+
+switch (opcoes)
+{
+case 1:
+    printf("Comparação de Cartas:\n");
 
 printf("População: Carta %d venceu (%d)\n",
 (Populacao > Populacao2) ? 1 : 2,
-(Populacao > Populacao2)  
-);
-
+(Populacao > Populacao2));
+    break;
+case 2:
+printf("Comparação de Cartas:\n");
 printf("Área: Carta %d venceu (%d)\n",
 (Area > Area2) ? 1 : 2,
 (Area > Area2)
 );
-
+    break;
+case 3:
+printf("Comparação de Cartas:\n");
 printf("PIB: Carta %d venceu (%d)\n",
 (PIB > PIB2) ? 1 : 2,
 (PIB > PIB2)
 );
 
+break;
+case 4:
+printf("Comparação de Cartas:\n");
 printf("Pontos Turísticos: Carta %d venceu (%d)\n",
 (Pontos_Turisticos > Pontos_Turisticos2) ? 1 : 2,
 (Pontos_Turisticos > Pontos_Turisticos2)
 );
-
-printf("Densidade Populacional: Carta %d venceu (%d)\n",
+break;
+case 5:
+printf("Comparação de Cartas:\n");
+printf("Densidade Demográfica: Carta %d venceu (%d)\n",
 (densidade < densidade2) ? 1 : 2,
 (densidade < densidade2)
 );
 
-printf("PIB per Capita: Carta %d venceu (%d)\n",
-(capita > capita2) ? 1 : 2,
-(capita > capita2)
-);
 
-printf("Super Poder: Carta %d venceu (%d)\n",
-(superpoder1 > superpoder2) ? 1 : 2,
-(superpoder1 > superpoder2)
-);
+break;
+}
 
 return 0;
 }
